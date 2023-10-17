d3408b2767a1695bb169cce8d9da581ca075c397
Isabella0038 <142461650+Isabella0038@users.noreply.github.com>
2023-10-17 18:48:54 -0500

    Add files via upload

---
+++ xml/xml/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace xml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(125, 87);
+            this.btnCrear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(136, 63);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear XML";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(445, 87);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(136, 63);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Leer XML";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 183);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 228);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(125, 439);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(136, 63);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(445, 439);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(136, 63);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(294, 105);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(106, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 562);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCrear);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
---
+++ xml/xml/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace xml
+{
+    public partial class Form1 : Form
+    {
+        private List<person> p1;
+        private string xmlFilePath = Environment.CurrentDirectory + "\\personas.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+            p1 = new List<person>();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            p1.Add(new person() { id = 1, nombre = "Isabela" });
+            p1.Add(new person() { id = 2, nombre = "Andres" });
+
+            SaveToXml();
+            MessageBox.Show("Creado");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            LoadFromXml();
+        }
+
+      
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                p1[selectedIndex].nombre = txtNombre.Text;
+                SaveToXml();
+                LoadFromXml();
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                p1.RemoveAt(selectedIndex);
+                SaveToXml();
+                LoadFromXml();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                txtNombre.Text = p1[selectedIndex].nombre;
+            }
+        }
+
+        private void LoadFromXml()
+        {
+            XmlSerializer serial = new XmlSerializer(typeof(List<person>));
+            using (System.IO.FileStream fs = new FileStream(xmlFilePath, FileMode.Open, FileAccess.Read))
+            {
+                p1 = serial.Deserialize(fs) as List<person>;
+            }
+            dataGridView1.DataSource = p1;
+        }
+
+        private void SaveToXml()
+        {
+            XmlSerializer serial = new XmlSerializer(typeof(List<person>));
+            using (System.IO.FileStream fs = new FileStream(xmlFilePath, FileMode.Create, FileAccess.Write))
+            {
+                serial.Serialize(fs, p1);
+            }
+        }
+
+    }
+}
